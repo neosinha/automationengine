c12187c1ced5146ad590b11d70267ac8307a0b54
nsinha <nsinha@10.102.130.189>
2017-03-29 11:36:31 -0700

    Test commit

---
+++ AutomationEngine/AutoEngine/AutomationApplication.py
@@ -0,0 +1,17 @@
+'''
+Created on Mar 29, 2017
+
+@author: Navendu Sinha
+'''
+
+class MyClass(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
+        
