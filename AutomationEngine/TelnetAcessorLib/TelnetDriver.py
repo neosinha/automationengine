67b0ff794ddd803e309e28d7013f142b82034787
smcochra <smcochra@gmail.com>
2017-04-12 15:34:27 -0700

    whitespace cleanup

--- AutomationEngine/TelnetAcessorLib/TelnetDriver.py
+++ AutomationEngine/TelnetAcessorLib/TelnetDriver.py
@@ -28,30 +28,30 @@ class TelnetDriver(object):
         self.debug('Hello (telnet) World')
         if console:
             self.open(console)
-            
+
     def set_debug_flag(self, flag):
         """
         Enable/Disable debug messaging
         + flag - True/False
         """
         self.debugFlag = flag
-        
+
     def open(self, console):
         self.t = telnetlib.Telnet()
 
         self._ip = console.split(':')[0]
         self._port = int(console.split(':')[1])
-        
+
         self.debug("Attempting to open connection with IP: '%s' Port: '%s'" % (self._ip, self._port))
         self.t.open(self._ip, self._port)
         self.debug("Session opened!")
-        
+
     def send(self, data):
         """
         + data - string to push to socket
         """
         self.t.write(data)
-        
+
     def expect_old(self, matchlist, timeout=5):
         """
         Returns tupple (idx, mtext, buf, timeout)
@@ -60,20 +60,20 @@ class TelnetDriver(object):
         + buf - buffer from time of expect starts to timeout/match
         """
         self.t.cookedq = ''
-        
+
         if not isinstance(matchlist, list):
             matchlist = [matchlist]
-        
+
         idx, mtext, buf = self.t.expect(matchlist, timeout=timeout)
-            
+
         return (idx, mtext, buf)
-    
+
     def expect(self, matchlist, timeout=5):
         """
         Accepts:
         + matchlist - list of values to match against
         + timeout - seconds before function should return if match is not met
-        
+
         Returns dictionary:
         + 'buffer' - list of dictionaries in format:
                 [{time1: [buffer_line_1, buffer_line_2]},
@@ -86,27 +86,27 @@ class TelnetDriver(object):
         """
         if not isinstance(matchlist, list):
             matchlist = list(matchlist)
-            
+
         # convert timeout to milliseconds
         timeout *= 1000
-    
+
         # define list of dictionaries;
         # list insures order is perserved rather than
         # sorting down the road...
         running_buf = []
         # remember last line of buffer to append to first line of next buf
         last_line_buf = ''
-        
+
         # matchidx initialized to 0
         midx = -1
         # index of timestamp we are iterating with
         tidx = 0
-        
+
         # compile regex's once to save processing
         compiled_regex = []
         for regex in matchlist:
             compiled_regex.append(re.compile(regex))
-        
+
         start_time = self.get_time()
         while self.get_time() - start_time < timeout:
             buf = self.t.read_very_eager()
@@ -114,12 +114,12 @@ class TelnetDriver(object):
                 # create list of lines associated with buffer
                 buf = buf.replace('\r', '')
                 buf_list = buf.split('\n')
-                
+
                 # first line is really cut off part from last line
                 last_idx = len(buf_list) - 1
                 buf_list[0] = last_line_buf + buf_list[0]
                 last_line_buf = buf_list[last_idx]
-                
+
                 # buffer to remember
                 timestamp = self.get_time()
                 running_buf.append({timestamp: []})
@@ -130,7 +130,7 @@ class TelnetDriver(object):
                     # don't append last item
                     if i < last_idx:                     
                         running_buf[tidx][timestamp].append(buf_list[i])
-                    
+
                     # iterate thru matchlist to look for matches in this line
                     for idx in range(len(compiled_regex)):
                         # compute regex
@@ -142,18 +142,18 @@ class TelnetDriver(object):
                                     'xtime': self.get_time() - start_time,
                                     'midx': idx,
                                     'mobj': mobj} 
-                        
+
                 tidx += 1
 
         xtime = self.get_time()() - start_time
         return {'buffer': running_buf, 'xtime': xtime, 'midx': midx, 'mobj': mobj}     
-        
+
     def close(self):
         """
         Close opened session
         """
         self.t.close()
-        
+
     def get_time(self):
         """
         Returns integer time in milliseconds
