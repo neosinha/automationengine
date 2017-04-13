1d28292b84f2cffc1451a648ba9926955d3d1c57
smcochra <smcochra@gmail.com>
2017-04-13 10:08:15 -0700

    testing new access rights

--- AutomationEngine/TelnetAcessorLib/TelnetAccessor.py
+++ AutomationEngine/TelnetAcessorLib/TelnetAccessor.py
@@ -39,7 +39,7 @@ class TelnetAccessor(object):
 
     def getlastmatchobj(self):
         """
-        Returns last RE match object
+        Returns last RE match object from expect
         """
         return self.matchobj
 
