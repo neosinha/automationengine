25a570ecbc9c0f223960e37952fa23a6011554c4
NeoSinha <neosinha@10.102.130.189>
2017-04-18 11:23:10 -0700

    # WARNING: head commit changed in the meantime
    Pushing updates

---
+++ AutomationEngine/AutoEngine/AutomationSequence.py
@@ -0,0 +1,250 @@
+'''
+Created on Mar 30, 2017
+
+@author: nsinha
+'''
+
+from collections import OrderedDict
+import re
+
+
+class ProcessSequenceStep(object):
+    '''
+    classdocs
+    '''
+
+    __processname = None
+    __sequenceSteps = []
+
+    def __init__(self, processname):
+        """
+        Initiallizes the Process Sequence Step with a processname 
+        """
+        self.__processname = processname
+        self.__sequenceSteps = {}
+
+    def addProcessSequenceStep(self, step):
+        """
+        Adds Sequence Step
+        """
+        self.__sequenceSteps.append(step)
+
+    def getProcessSequenceSteps(self):
+        """
+        Returns the sequence Steps
+        """
+        return self.__sequenceSteps
+
+
+class SequenceStep(object):
+    '''
+    classdocs
+    '''
+
+    __name = None
+    __cmdObject = OrderedDict()
+
+    parsedict = None
+
+    def __init__(self, sequenceName=None):
+        '''
+        Constructor
+        '''
+        self.__name = sequenceName
+        self.parsedict = {}
+
+    def addParseExtract(self, varname, regexp):
+        """
+        Add a ParseExtract Object
+        """
+        self.parsedict[varname] = regexp
+
+    def addSequenceStep(self, stepName, cmdObject):
+        """
+        Adds a command object with a stepName 
+        """
+        self.__cmdObject[stepName] = cmdObject
+
+    def getSequenceStep(self, stepName):
+        """
+        Returns the Sequence step object corresponding to stepname 
+        """
+
+    def getSequenceSteps(self):
+        """
+        Returns the SequenceStep steps ordered dictionary
+        """
+        return self.__cmdObject
+
+
+class ParseExtract(object):
+    """
+    Handle buffer parsing from pre-defined
+    regular expressions
+    """
+    _regex_dict = {}
+    _expect_dict = {}
+    _callback_dict = {}
+
+    def __init__(self, key=None, regex=None, expect=None, matchcallback=None, notmatchcallback=None):
+        if key is not None:
+            self._regex_dict[key] = regex
+            if expect is not None:
+                self._expect_dict[key] = expect
+                if matchcallback is not None:
+                    self._callback_dict["True"] = matchcallback
+                if notmatchcallback is not None:
+                    self._callback_dict["False"] = notmatchcallback
+
+    def getparseextract(self):
+        """
+        Returns the dict of the parse extract defintions
+        """
+        return self._regex_dict
+
+    def addparser(self, key, regex, expect, matchcallback=None, notmatchcallback=None):
+        """
+        Add regular expression to be stored
+        and called upon during parsing activity.
+
+        After defining regex, use extract for matching
+
+        + key - name of regex
+        - regex - regular expression
+        """
+        self._regex_dict[key] = re.compile(regex)
+        self._expect_dict[key] = expect
+        if matchcallback is not None:
+            self._callback_dict["True"] = matchcallback
+        if notmatchcallback is not None:
+            self._callback_dict["False"] = notmatchcallback
+
+    def extract(self, buffer, *keys):
+        """
+        Extract regex from buffer from predefined
+        key using re.search()
+
+        Returns dictionary in format of {key: matchtext}
+
+        If regex included group(s), matchtext will be last
+        group matched
+        """
+        returndict = {}
+
+        for key in keys:
+            returndict[key] = None
+
+            if key not in self._regex_dict:
+                continue
+
+            regexresult = re.search(self._regex_dict[key], buffer)
+
+            if regexresult:
+                # length of groups will indicate last item to group and return
+                num_groups = len(regexresult.groups())
+                returndict[key] = regexresult.group(num_groups)
+
+        return returndict
+
+    def extractkeys(self, buffer):
+        """
+        Extract regex from buffer from predefined
+        key using re.search()
+
+        Returns dictionary in format of {key: matchtext}
+
+        If regex included group(s), matchtext will be last
+        group matched
+        """
+        returndict = {}
+
+        for key in self._regex_dict.keys():
+            regexresult = re.search(self._regex_dict[key], buffer)
+
+            if regexresult:
+                # length of groups will indicate last item to group and return
+                num_groups = len(regexresult.groups())
+                matchState = False
+                if regexresult.group(num_groups) == self._expect_dict[key]:
+                    matchState = True
+
+                callbackkey = str(matchState)
+                print "CallBackKey: %s" % (callbackkey)
+                # contrsuct a dict
+                keyobj = {}
+                keyobj['value'] = str(regexresult.group(num_groups)).strip()
+                keyobj['match'] = matchState
+
+                if 'True' in self._callback_dict:
+                    keyobj['callback'] = self._callback_dict['True']
+                if 'False' in self._callback_dict:
+                    keyobj['callback'] = self._callback_dict['False']
+                returndict[key] = keyobj
+
+        # print "Keys: %s" % (returndict)
+        return returndict
+
+
+# def test_parse_extract():
+#     buffer = """
+#         hi this is james and this
+#         is my test_parse_extract module code.
+#         version = 10.125.3:A2
+#         we can create regex's and take action
+#         based on the key value defined
+#         by the user
+#     """
+#     parse = ParseExtract()
+#
+#     parse.addparser('james', 'hi.this.*(ja.es)')
+#     parse.addparser('version', 'version.=.(.*)')
+#     parse.addparser('module', 'module')
+#
+#     print parse.extract(buffer, 'james', 'version', 'module')
+#
+# test_parse_extract()
+
+
+class CommandObject(object):
+    """
+    Command Object Definition
+    """
+
+    cmdstr = None
+    timeout = None
+    prompt = None
+    buffer = None
+
+    parseex = None
+
+    def __init__(self, cmdstr, timeout, prompt):
+        """
+        Command Object 
+        """
+        self.cmdstr = "%s\r" % (cmdstr)
+        self.timeout = timeout
+        self.prompt = prompt
+
+    def getCommand(self):
+        """
+        Get Command
+        """
+        cmdobj = {}
+        cmdobj["cmd"] = self.cmdstr
+        cmdobj["timeout"] = self.timeout
+        cmdobj["prompt"] = self.prompt
+        if self.parseex is not None:
+            cmdobj["parseext"] = self.parseex
+        return cmdobj
+
+    def addParseExtract(self, parseex):
+        """
+        Adds parse extract object
+        + parseex
+        """
+        self.parseex = parseex
+
+    def getParseExtract(self):
+        """
+        """
+        return self.parseex
