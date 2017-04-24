c8d316d0bd681ee976c020a487aaebab9f0ee786
mcontrer <miguel19896@hotmail.com>
2017-04-24 11:03:02 -0700

    New Updates

--- AutomationEngine/QueryTool/Main.py
+++ AutomationEngine/QueryTool/Main.py
@@ -10,21 +10,42 @@ Created on April 14, 2017
 import QueryTool
 import datetime
 import cherrypy as QueryServer
-
-
-class QueryAccessor(object):
-
-    def __init__(self):
-        self.q = QueryTool.QueryTool()
-
-    def QueryInitiliaze(self):
-        QueryServer.config.update({'server.socket_host': '127.0.0.1', 'server.socket_port': 9100,})
-        QueryServer.quickstart(self.q)
+import os
 
 
 if __name__ == "__main__":
+    """
+    This initializes CherryPy services
+    + self - no input required
+    """
 
     print "Intializing!"
-    QueryAccessor().QueryInitiliaze()
 
+    portnum = 9100
+
+    # start the QeueryServer
+    QueryServer.config.update({'server.socket_host' : '127.0.0.1',
+                            'server.socket_port': portnum,
+                            'server.socket_timeout': 600,
+                            'server.thread_pool' : 8,
+                            'server.max_request_body_size': 0
+                            })
+
+    wwwPath = os.path.join(os.getcwd(),'www')
+
+    print wwwPath
+
+    staticdir = './www'
+
+    print staticdir
+
+    conf = {
+            '/': {
+                'tools.sessions.on': True,
+                'tools.staticdir.on': True,
+                'tools.staticdir.dir': wwwPath
+            }
+        }
+
+    QueryServer.quickstart(QueryTool.QueryTool(dbaddress="10.30.5.203:27017", path= wwwPath), '/', conf)
 
